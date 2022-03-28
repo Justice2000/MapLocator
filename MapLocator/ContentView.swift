495c763d4431f4868922549dde7d1a75f4ec9b3f
Justice2000 <91248448+Justice2000@users.noreply.github.com>
2022-03-27 22:09:59 -0400

    first commit

--- MapLocator/ContentView.swift
+++ MapLocator/ContentView.swift
@@ -6,11 +6,69 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var mapregion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack
+        {
+            Map(coordinateRegion: $mapregion)
+            
+            VStack
+            {
+                HStack
+                            {
+                                Button(action: {})
+                                            {
+                                                Text("Cinema")
+                                                    .bold()
+                                                    .foregroundColor(.white)
+                                                    .padding(.all,20)
+                                                    .padding([.leading,.trailing], 28)
+                                                    .background(Color.blue)
+                                                    .cornerRadius(20)
+                                            }
+                                Button(action: {})
+                                            {
+                                                Text("Malls")
+                                                    .bold()
+                                                    .foregroundColor(.white)
+                                                    .padding(.all,20)
+                                                    .padding([.leading,.trailing], 40)
+                                                    .background(Color.blue)
+                                                    .cornerRadius(20)
+                                            }
+                            }
+                HStack
+                            {
+                                Button(action: {})
+                                            {
+                                                Text("Parks")
+                                                    .bold()
+                                                    .foregroundColor(.white)
+                                                    .padding(.all,20)
+                                                    .padding([.leading,.trailing], 40)
+                                                    .background(Color.blue)
+                                                    .cornerRadius(20)
+                                            }
+                                Button(action: {})
+                                            {
+                                                Text("Resturants")
+                                                    .bold()
+                                                    .foregroundColor(.white)
+                                                    .padding(.all,20)
+                                                    .padding([.leading,.trailing], 20)
+                                                    .background(Color.blue)
+                                                    .cornerRadius(20)
+                                            }
+                            }
+            }
+            
+            
+        }
+        
     }
 }
 
