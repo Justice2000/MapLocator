2290c32bcb767fff12fbc85b52b291e9cfdc696d
Justice2000 <91248448+Justice2000@users.noreply.github.com>
2022-03-27 21:41:12 -0400

    Initial Commit

---
+++ MapLocator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapLocator
+//
+//  Created by Justice Ekeke on 27/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MapLocator/MapLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocatorApp.swift
+//  MapLocator
+//
+//  Created by Justice Ekeke on 27/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
